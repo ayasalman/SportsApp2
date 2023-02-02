f63c95ccd013dcd379151b1a5b27f41327d11906
ayasalman <ayas23830@gmail.com>
2023-02-02 16:16:27 +0200

    tabBar Edit

---
+++ SportsApp/TabBarVC.swift
@@ -0,0 +1,26 @@
+//
+//  TabBarVC.swift
+//  SportsApp
+//
+//  Created by Aya on 02/02/2023.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    func ChangeRadiusOfTabBar()
+    {
+        
+    }
+    
+
+}
